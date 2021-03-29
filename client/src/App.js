c020131e830a1248e0afaaee02f8ee6f5ead0db3
Alex Hong <seongkwh@andrew.cmu.edu>
2021-03-29 16:30:35 -0400

    docker not fully functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+  callAPI() {
+      fetch('http://localhost:9000/testAPI')
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  testAPI = () => {
+    return "Test API";
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <a>
+            Hey {this.testAPI()} {this.state.apiResponse}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
