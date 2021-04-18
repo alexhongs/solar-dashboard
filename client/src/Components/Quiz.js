28b77b32623d522064d76a2b1e659d21907e4ebc
Roy <royxuhan@gmail.com>
2021-04-17 21:24:02 -0400

    Button selection

--- client/src/Components/Quiz.js
+++ client/src/Components/Quiz.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useStoreState, useStoreActions } from 'easy-peasy';
 
-function getQuizContent(quizStep, incrementQuizStep) {
+function getQuizContent(selected, quizStep, quizResult, setQuizResult) {
   switch(quizStep) {
     case 0: 
       return (
@@ -10,13 +10,13 @@ function getQuizContent(quizStep, incrementQuizStep) {
 
           <div className='quiz-row row'>
             <div className='six columns'>
-              <button type='button' class='quiz-button btn bg-transparent'
-              onClick={() => incrementQuizStep('Home')}> Home </button>
+              <button type='button' class={selected === 'Home' ? 'selected quiz-button btn bg-transparent' : 'quiz-button btn bg-transparent'}
+              onClick={() => setQuizResult('Home')}> Home </button>
             </div>
 
             <div className='six columns'>
-              <button type='button' class='quiz-button btn bg-transparent'
-              onClick={() => incrementQuizStep('Business')}> Business </button>
+              <button type='button' class={selected === 'Business' ? 'selected quiz-button btn bg-transparent' : 'quiz-button btn bg-transparent'}
+              onClick={() => setQuizResult('Business')}> Business </button>
             </div>
           </div>
         </>
@@ -27,15 +27,13 @@ function getQuizContent(quizStep, incrementQuizStep) {
           <h1> What county is your home in? </h1>
 
           <div className='quiz-row row'>
-            <div className='six columns'>
-              <button type='button' class='quiz-button btn bg-transparent'
-              onClick={() => incrementQuizStep('Home')}> Home </button>
-            </div>
-
-            <div className='six columns'>
-              <button type='button' class='quiz-button btn bg-transparent'
-              onClick={() => incrementQuizStep('Business')}> Business </button>
-            </div>
+            <form>
+              <label>
+                Name:
+                <input type="text" name="name" />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
           </div>
         </>
       )
@@ -43,7 +41,11 @@ function getQuizContent(quizStep, incrementQuizStep) {
 }
 
 function Quiz() {
+  const selected = useStoreState(state => state.selected);
   const quizStep = useStoreState(state => state.quizStep);
+  const quizResult = useStoreState(state => state.quizResult);
+  const setQuizResult = useStoreActions((actions) => actions.setQuizResult);
+  const skipQuizStep = useStoreActions((actions) => actions.skipQuizStep);
   const incrementQuizStep = useStoreActions((actions) => actions.incrementQuizStep);
 
   return (
@@ -68,23 +70,19 @@ function Quiz() {
         </div>
 
         <div className='nine columns main-col'>
-          {getQuizContent(quizStep, incrementQuizStep)}
+          {getQuizContent(selected, quizStep, quizResult, setQuizResult)}
         </div>
 
         <div className='quiz-submit-row twelve columns main-col'>
-          <p> Skip <i class='icon-right-circle-1' /></p>
+          <button type='button' class='skip-button btn' 
+          onClick={() => skipQuizStep()}> Skip </button>
+
+          <button type='button' class='next-button btn' 
+          onClick={() => incrementQuizStep()}> <span STYLE="font-size:24pt">&#8594;</span> </button>
         </div>
       </div>
     </section>
   );
 }
-
-// class Quiz extends Component {
-//   render() {
-//     return (
-      
-//     )
-//   }
-// }
 
 export default Quiz
--- client/src/services/store.js
+++ client/src/services/store.js
@@ -1,27 +1,30 @@
-import { createStore, action, persist } from 'easy-peasy';
+import { createStore, action } from 'easy-peasy';
 
-const store = createStore(
-  persist({
-    quizStep: 0,
-    quizResult: {
-      0: 'unanswered',
-      1: 'unanswered',
-      2: 'unanswered',
-      3: 'unanswered',
-      4: 'unanswered',
-      5: 'unanswered',
-      6: 'unanswered',
-      7: 'unanswered'
-    },
-    incrementQuizStep: action((state, payload) => {
-      state.quizResult[state.quizStep] = payload;
-      state.quizStep += 1;
-    }),
+const store = createStore({
+  selected: '',
+  quizStep: 0,
+  quizResult: {
+    0: 'unanswered',
+    1: 'unanswered',
+    2: 'unanswered',
+    3: 'unanswered',
+    4: 'unanswered',
+    5: 'unanswered',
+    6: 'unanswered',
+    7: 'unanswered'
+  },
+  setQuizResult: action((state, payload) => {
+    state.selected = payload;
+    state.quizResult[state.quizStep] = payload;
   }),
-  {
-    version: 1,
-  }
-)
+  skipQuizStep: action((state) => {
+    state.quizResult[state.quizStep] = 'unanswered';
+    state.quizStep += 1;
+  }),
+  incrementQuizStep: action((state, payload) => {
+    state.quizStep += 1;
+  }),
+})
 
 
 export default store
