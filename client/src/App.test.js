a1f44c5ed5d56161cb027b03f5df41cc10e95f30
Roy <royxuhan@gmail.com>
2021-04-23 00:04:50 -0400

    Test fix

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {createStore, StoreProvider} from 'easy-peasy';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const model = {
+  quizStep: 0,
+  quizResult: {
+    0: 'Home',
+    1: 'Allegheny',
+    2: 'Test',
+    3: 'Test',
+    4: 'Test',
+    5: 'Test',
+    6: 'Test',
+  },
+};
+
+describe('Customer page', () => {
+  const store = createStore(model, {
+    mockActions: true,
+  });
+
+  it('Should have the correct tabs in the nav bar', () => {
+    const app = (
+      <StoreProvider store={store}>
+        <App />
+      </StoreProvider>
+    );
+
+    const {getByTestId} = render(app);
+    const element = screen.getByText(/Did you know Western PA, there are/i);
+
+    expect(element).toBeInTheDocument();
+  });
+})
