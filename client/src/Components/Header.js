596c13320b3a32547cde237488b12f1b6eb44a5a
Roy <royxuhan@gmail.com>
2021-05-06 23:13:58 -0400

    UI fixes

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
       <header id="home">
         <div className="row banner">
           <div className="banner-text">
-            <h2 className="responsive-headline">Did you know Western PA, there are</h2>
+            <h2 className="responsive-headline">Did you know in the U.S, there are</h2>
             <img className="tagline" src={tagline} alt="tagline" />
             <img className="tagline2" src={tagline2} alt="tagline" />
           </div>
